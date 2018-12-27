1bebe4f1ad2b7ca3c65a8194be5038d2f08f51e2
siroyan <1517053@ed.tus.ac.jp>
2018-12-27 11:56:14 +0900

    [modify] delete asciiCodeArray

--- src/CCode.java
+++ src/CCode.java
@@ -11,7 +11,7 @@ public class CCode{
     public void getCCode(String rawData){
         ColorNumber colorNumber = new ColorNumber();
         colorNumber.setRawData(rawData);
-        byte[][] rgb = colorNumber.getColorNumber();
+        byte[] rgb = colorNumber.getColorNumber();
         try {
             BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_3BYTE_BGR);
             Graphics g = img.getGraphics();
@@ -19,14 +19,14 @@ public class CCode{
             for(int i = 0; i < 100; i = i + 10){
                 for(int j = 0; j < 100; j = j + 10){
                     if(cursor < rgb.length){
-                        g.setColor(new Color((int)rgb[cursor][0], (int)rgb[cursor][1], (int)rgb[cursor][2]));
+                        g.setColor(new Color((int)rgb[cursor], (int)rgb[cursor+1], (int)rgb[cursor+2]));
                         g.fillRect(j, i, 10, 10);
-                        cursor++;
+                        cursor = cursor + 3;
                     }
                 }
             }
             g.dispose();
-            ImageIO.write(img, "png", new File("sample.png"));
+            ImageIO.write(img, "png", new File("../sample.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/ColorNumber.java
+++ src/ColorNumber.java
@@ -3,23 +3,16 @@ import java.nio.charset.StandardCharsets;
 public class ColorNumber{
     private String rawData;
     private byte[] asciiCode;
-    private byte[][] colorNumber;
     ColorNumber(){
         
     }
     public void setRawData(String data){
         rawData = data;
     }
-    public byte[][] getColorNumber(){
+    public byte[] getColorNumber(){
         byte[] asciiCodeTemp = rawData.getBytes(StandardCharsets.UTF_8);
         asciiCode = new byte[asciiCodeTemp.length + 3 - asciiCodeTemp.length % 3];
         System.arraycopy(asciiCodeTemp, 0, asciiCode, 0, asciiCodeTemp.length);
-        colorNumber = new byte[asciiCode.length / 3][3];
-        for(int i = 0; i < asciiCode.length / 3; i++){
-            for(int j = 0; j < 3; j++){
-                colorNumber[i][j] = asciiCode[i * 3 + j];
-            }
-        }
-        return colorNumber;
+        return asciiCode;
     }
 }
